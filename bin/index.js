ac61b57654b49a4e2339e3d7e5958a293f74032b
duongtdn <duongtdn@gmail.com>
2022-07-11 23:10:42 +0700

    add sync command

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
-const zip = require('../lib/zip/zip');
+const zip = require("../lib/zip/zip");
+const sync = require("../lib/sync/sync");
 
 // parsing arguments
 const [,, ...args] = process.argv
 const path = process.cwd()
 const options = {
-  verbose: args.indexOf('verbose') === -1 ? false : true
+  verbose: args.indexOf("verbose") === -1 ? false : true
 }
 
 // calling subroutines
 switch (args[0]) {
-  case 'zip':
-    zip(path, options).then(v => console.log(v)).catch( err => console.log(err))
-    break
+  case "zip":
+    zip(path, options).then(v => console.log(v)).catch( err => console.log(err));
+    break;
+  case "sync":
+    sync(args[1]);
+    break;
   default:
-    help()
-    break
+    help();
+    break;
 }
 
 function help() {
-  console.log('Syntax: realm <action> [verbose]')
+  console.log("Syntax: realm <action> [verbose]")
 }
