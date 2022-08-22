6ea907f9a7a450a97e0b14550536a381106514ff
duongtdn <duongtdn@gmail.com>
2022-08-22 20:30:12 +0700

    add project sync command

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
-const zip = require('../lib/zip')
+const zip = require("../lib/zip/zip");
+const project = require("../lib/project");
 
 // parsing arguments
 const [,, ...args] = process.argv
 const path = process.cwd()
 const options = {
-  verbose: args.indexOf('verbose') === -1 ? false : true
+  verbose: args.indexOf("verbose") === -1 ? false : true
 }
 
 // calling subroutines
 switch (args[0]) {
-  case 'zip':
-    zip(path, options).then(v => console.log(v)).catch( err => console.log(err))
-    break
+  case "zip":
+    zip(path, options).then(v => console.log(v)).catch( err => console.log(err));
+    break;
+  case "project":
+    project(args.slice(1));
+    break;
   default:
-    help()
-    break
+    help();
+    break;
 }
 
 function help() {
-  console.log('Syntax: realm <action> [verbose]')
+  console.log("Syntax: realm <action> [verbose]")
 }
